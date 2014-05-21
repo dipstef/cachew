e84270f67f6da9aadf2a446a8dd42db5a92df92f
dipstef <dipstef@gmail.com>
2014-05-21 13:02:40 +0100

    Modified build file

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
-from distutils.command.build_py import build_py
 from distutils.core import setup
-import os
-import shutil
 
 VERSION = '0.1'
 
@@ -11,19 +8,6 @@ desc = """Sqlite response cache for httpy. Used together with an httpy_client wi
 name = 'httpy_cache'
 
 
-class CacheInitFileCopy(build_py):
-
-    def run(self):
-        build_py.run(self)
-
-        build_file_path = os.path.join(self.build_lib, 'httpy', 'cache', 'cache.sql')
-
-        if os.path.exists(build_file_path):
-            os.remove(build_file_path)
-
-        cache_init_path = os.path.join(os.path.dirname(__file__), 'httpy', 'cache', 'cache.sql')
-        shutil.copy(cache_init_path, build_file_path)
-
 setup(name=name,
       version=VERSION,
       author='Stefano Dipierro',
@@ -32,6 +16,6 @@ setup(name=name,
       description=desc,
       packages = ['httpy.cache'],
       platforms=['Any'],
-      cmdclass={'build_py': CacheInitFileCopy},
+      package_data={'': ['cache.sql']},
       requires=['urlo', 'httpy', 'quelo']
 )
